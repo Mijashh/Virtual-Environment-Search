91cfb43f1d46e5891c5995d12f149981a1018793
Mijash Adhikari <amijash@gmail.com>
2024-01-21 11:55:38 +0545

    Virtual environment are checked and their dependencies are displayed

--- scripting.py
+++ scripting.py
@@ -1,25 +1,38 @@
 import os
+import subprocess
 
 
 def listdirectory(dirpath, depth=0):
+    a=False
     for item in os.listdir(dirpath):
         
         item_path=os.path.join(dirpath,item)
         
-        if os.path.isdir(item_path):
+        if os.path.isdir(item_path) and item!=".git":
             print(" "*depth+item)
+            a=searchforenv(item,item_path,depth)
+            if a is True:
+                break
             listdirectory(item_path,depth+5)
 
-        
+def searchforenv(item,item_path,depth):
+    for items in item:
+        reqfile=os.path.join(item_path,"pyvenv.cfg")
+        if os.path.isfile(reqfile):
+            print(" "*depth + "Virtual environment present!!!")
+            dependencies(item_path)
+            return True
+
+def dependencies(item_path):
+    bash_command=item_path+"/bin/python -m pip list"
+    subprocess.run(bash_command,shell=True,executable="/bin/zsh")
+         
             
         
 
-directo= input("Enter directory name to search: ")
-directory=os.path.join("/home/mijashadhikari",directo)
-os.chdir(directory)
-current_directory = os.getcwd()
-# # print(f'Current working directory is now: {current_directory}')
-# directoryitems=os.listdir(current_directory)
-# # for items in directoryitems:
-# #     print(items)
-listdirectory(current_directory)
+if __name__=="__main__":
+    directo= input("Enter directory name to search: ")
+    directory=os.path.join("/home/mijashadhikari",directo)
+    os.chdir(directory)
+    current_directory = os.getcwd()
+    listdirectory(current_directory)
