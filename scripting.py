07273fc5eae7869f4ff6124c3478c271f265c788
Mijash Adhikari <amijash@gmail.com>
2024-01-23 16:12:56 +0545

    fix: show hidden directories as well

--- scripting.py
+++ scripting.py
@@ -4,7 +4,7 @@ import subprocess
 
 def list_directory(root):
     print("\nThe available directories are: \n")
-    subprocess.run(["ls", root])
+    subprocess.run(["ls","-a", root])
 
 
 def search_for_env(directory):
@@ -20,11 +20,11 @@ def show_virtual(a_virtual):
     if len(a_virtual) == 0:
         print("No virtual environments present in selected directory.")
         while True:
-            ask_user = input("\nDo you want to search in another directory??  (y/N) : ")
-            if ask_user == "y" or ask_user == "Y":
+            ask_user = input("\nDo you want to search in another directory??  (Y/N) : ")
+            if ask_user.lower() in {"y","yes"}:
                 main()
                 break
-            elif ask_user in {"n", "N"}:
+            elif ask_user.lower() in {"n", "no"}:
                 print("\nExited Successfully!!!")
                 break
             else:
@@ -44,8 +44,8 @@ def show_virtual(a_virtual):
                     )
                 )
                 if position <= len(a_virtual):
-                    index=position-1
-                    manage_virtual(a_virtual[index], a_virtual,index)
+                    index = position - 1
+                    manage_virtual(a_virtual[index], a_virtual, index)
                     break
 
                 else:
@@ -54,36 +54,36 @@ def show_virtual(a_virtual):
                 print("\nEnter a valid integer value\n")
 
 
-def manage_virtual(dirpath, a_virtual,index):
+def manage_virtual(dirpath, a_virtual, index):
     print(f"\nYou are inside the virtual environment named {os.path.basename(dirpath)}")
     while True:
-        qn = input("\nDo you want to checkout the modules installed? (y/N): ")
+        qn = input("\nDo you want to checkout the modules installed? (Y/N): ")
 
-        if qn == "y" or qn == "Y":
+        if qn.lower() in {"y","yes"}:
             print("\n")
             show_dependencies(dirpath)
             break
-        elif qn not in {"n", "N"}:
+        elif qn.lower not in {"n", "no"}:
             print("Invalid input, enter again!!! ")
         else:
             break
 
     while True:
-        ask = input("Do you want to delete the Virtual Environment?? (y/N): ")
-        if ask == "y" or ask == "Y":
+        ask = input("Do you want to delete the Virtual Environment?? (Y/N): ")
+        if ask.lower() in {"y","yes"}:
             del_virtualenv(dirpath)
             del a_virtual[index]
             break
-        elif ask not in {"n", "N"}:
+        elif ask.lower() not in {"n", "no"}:
             print("\nInvalid input. Enter again!!! ")
         else:
             break
     while True:
         ask_user = input("Exit program or select another virtual environment?? (E/s): ")
-        if ask_user in {"s", "S"}:
+        if ask_user.lower() in {"s"}:
             print("\n")
             show_virtual(a_virtual)
-        elif ask_user in {"e", "E"}:
+        elif ask_user.lower in {"e"}:
             print("Exited Successfully!!!")
             break
         else:
@@ -99,13 +99,13 @@ def show_dependencies(item_path):
             "\nDo you want to add or delete dependecies? Press S to skip (Add/Del/S): "
         )
 
-        if qn == "del" or qn == "Del":
+        if qn.lower() in {"del","d"}:
             delete_dependency(item_path)
             break
-        elif qn == "add" or qn == "Add":
+        elif qn.lower() in {"add","a"}:
             add_dependency(item_path)
             break
-        elif qn in {"s", "S"}:
+        elif qn.lower() in {"s"}:
             break
         else:
             print("Invalid input. Enter again!!!")
@@ -120,14 +120,14 @@ def delete_dependency(item_path):
 
 
 def add_dependency(item_path):
-    bash_check=item_path + "/bin/python -m pip list"
+    bash_check = item_path + "/bin/python -m pip list"
     result = subprocess.run(bash_check, capture_output=True, text=True, shell=True)
     package_name = input("\n Enter the name of the package to be installed: ")
     if package_name in result.stdout:
         print("\nPackage already installed. Enter another package!!!")
     else:
         bash_command = item_path + f"/bin/python -m pip install {package_name}"
-        subprocess.run(bash_command, shell=True)  
+        subprocess.run(bash_command, shell=True)
     print("\nThe dependency currently present in the virtual environment are: ")
     show_dependencies(item_path)
 
